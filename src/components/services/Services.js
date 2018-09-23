074805567fc50d50b749e4a8b49a4d87e9c70d35
nachomirsol <nachomirsol@gmail.com>
2018-09-24 00:58:43 +0200

    removing comments TODO refactor

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,45 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import ItemService from './ItemService';
 import { services } from '../../data/data.json';
 import "./Services.css";
 
-class Services extends Component {
-  constructor() {
-    super();
-    this.state = {
-      //services:services
-    }
-  }
+const Services = () => {
+  return (
+    <div className="col-md-5 services">
+      <h3>
+        <strong>Services</strong>
+      </h3>
 
-  render() {
-    return (
-      <div className="col-md-5 services">
-        <h3>
-          <strong>Services</strong>
-        </h3>
+      <div className="row">
 
-        <div className="row">     
-            {/*<ItemService /> */}
-            {services.map((item,i) => (
-              <div className="col-md-12" key={i}>
-                <h5><strong>{item.title}</strong></h5>
-                {item.service.map((element,index) => {
-                  return(
-                    <ItemService 
-                      key = {index}
-                      name = {element.name} 
-                      fee = {element.fee}
-                      currency = {element.currency}
-                      star = {element.star_rating}
-                    />
-                  )
-                })}
-              </div>
-            ))}        
-        </div>
+        {services.map((item, i) => (
+          <div className="col-md-12" key={i}>
+            <h5><strong>{item.title}</strong></h5>
+            {item.service.map((element, index) => {
+              return (
+                <ItemService
+                  key={index}
+                  name={element.name}
+                  fee={element.fee}
+                  currency={element.currency}
+                  star={element.star_rating}
+                />
+              )
+            })}
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Services;
